7b8c93d6c59cf508192409ea776aebb7bb757070
Nebbs82 <nathan.r.ebbesen@hotmail.com>
2025-03-09 21:14:36 -0500

    yay, it does work, and the readme is added

--- 02-Challenge/Develop/server/src/server.ts
+++ 02-Challenge/Develop/server/src/server.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+// import path from 'node:path';
+// import { fileURLToPath } from 'node:url';
 
 dotenv.config();
 
@@ -11,10 +11,11 @@ import routes from './routes/index.js';
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 // TODO: Serve static files of entire client dist folder
-app.use(express.static(`${__dirname}/client/dist`));
+app.use(express.static(`../client/dist`));
+console.log()
 
 // TODO: Implement middleware for parsing JSON and urlencoded form data
 app.use(express.json()); 
---
+++ README.md
@@ -0,0 +1,33 @@
+# Weather Service
+  
+
+
+
+## Description
+A weather service app using an API
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+bun run build && bun run start:dev
+
+## Usage
+To find 5-day weather forecasts for any city.
+
+
+
+## Contributing
+n/a
+
+## Tests
+bun run start && bun run start:dev to run locally or visit the deployed site. [Weather App](https://challenge-9-weather-forecast.onrender.com)
+
+## Questions
+If you have any questions, please reach out to me at [Nathan.R.Ebbesen@hotmail.com](mailto:Nathan.R.Ebbesen@hotmail.com).
+You can find my GitHub profile at [Nebbs82](https://github.com/Nebbs82).
